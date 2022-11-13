c21a1cf5cd9b4c60e365bbd3a77184bacbb34946
Duane <duane.brk@gmail.com>
2022-11-13 09:08:31 -0500

    Update NavBar

--- includes/header.php
+++ includes/header.php
@@ -32,18 +32,7 @@ require_once 'includes/session.php'
         </li>
         <li class="nav-item">
           <a class="nav-link" href="viewrec.php">Participants</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Options
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="view.php">View Record</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>        
+        </li>               
         </ul>        
      <div class="navbar-nav ml-auto">
       <?php 
